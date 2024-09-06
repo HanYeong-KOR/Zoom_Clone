e41334e3c028c0509fd932212565c7a25ad7b941
Hanyeong Choi <younghy96@naver.com>
2024-09-06 15:54:15 +0900

    240906 commit

--- src/public/js/socketIO/app.js
+++ src/public/js/socketIO/app.js
@@ -16,12 +16,18 @@ function handleRoomSubmit(event) {
 
 function handleMessageSubmit(event) {
     event.preventDefault();
-    const input = room.querySelector('input');
+    const input = room.querySelector('#msg input');
     const value = input.value;
     socket.emit('new_message', roomName, input.value, () => {
         sendMessage(`You : ${value}`);
     });
     input.value = '';
+}
+
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector('#name input');
+    socket.emit('nickname', input.value);
 }
 
 function showRoom() {
@@ -30,8 +36,11 @@ function showRoom() {
     const h3 = room.querySelector('h3');
     h3.innerText = `Room ${roomName}`;
 
-    const roomForm = room.querySelector('form');
-    roomForm.addEventListener('submit', handleMessageSubmit);
+    const msgForm = room.querySelector('#msg');
+    msgForm.addEventListener('submit', handleMessageSubmit);
+
+    const nameForm = room.querySelector('#name');
+    nameForm.addEventListener('submit', handleNicknameSubmit);
 }
 
 function sendMessage(message) {
@@ -43,12 +52,12 @@ function sendMessage(message) {
 
 welcomeForm.addEventListener('submit', handleRoomSubmit);
 
-socket.on('welcome', () => {
-    sendMessage('Someone joined!');
+socket.on('welcome', (user) => {
+    sendMessage(`${user} joined!`);
 });
 
-socket.on('bye', () => {
-    sendMessage('Someone left T.T');
+socket.on('bye', (user) => {
+    sendMessage(`${user} left T.T`);
 })
 
 socket.on('new_message', sendMessage);
--- src/socketIO_Server.js
+++ src/socketIO_Server.js
@@ -12,6 +12,7 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
+    socket['nickname'] = 'Anon';
     socket.onAny((event) => {
         console.log(`Socket Event : ${event}`);
     });
@@ -20,17 +21,19 @@ wsServer.on('connection', (socket) => {
         socket.join(roomName.payload);
         showRoom();
 
-        socket.to(roomName.payload).emit('welcome');
+        socket.to(roomName.payload).emit('welcome', socket.nickname);
     });
 
     socket.on('disconnecting', () => {
-        socket.rooms.forEach(room => socket.to(room).emit('bye'));
+        socket.rooms.forEach(room => socket.to(room).emit('bye', socket.nickname));
     });
 
     socket.on('new_message', (roomName, msg, done) => {
-        socket.to(roomName).emit('new_message', msg);
+        socket.to(roomName).emit('new_message', `${socket.nickname} : ${msg}`);
         done();
     });
+
+    socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
